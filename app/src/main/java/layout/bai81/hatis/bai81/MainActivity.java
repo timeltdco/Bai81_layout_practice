2e607b9d43565f37c647c7b912919bc5b70db20d
Hai Thanh <timeltdco@gmail.com>
2018-12-12 18:37:36 +0700

    Initial commit

---
+++ app/src/main/java/layout/bai81/hatis/bai81/MainActivity.java
@@ -0,0 +1,13 @@
+package layout.bai81.hatis.bai81;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.facebook_llide);
+    }
+}
